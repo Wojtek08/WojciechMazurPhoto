2bc76a7a52897fdcbc4b5f00198b7c626a15e43a
Wojtek08 <114952767+Wojtek08@users.noreply.github.com>
2023-06-07 20:08:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-var burger = document.getElementById("burger");
-var nav = document.getElementById("nav-ul");
-var first = document.getElementById("1st");
-var second = document.getElementById("2nd");
 var html = document.getElementById("html");
 
-burger.onclick = function () {
+burger.addEventListener('click', dropdown = function () {
+    let burger = document.getElementById("burger");
+    let nav = document.getElementById("nav-ul");
     if (nav.className == "open") {
         nav.className = "";
         burger.src = "assets/BurgerWhite.svg"
@@ -12,18 +10,14 @@ burger.onclick = function () {
         nav.className = "open";
         burger.src = "assets/CloseWhite.svg"
     };
-};
+});
 
-first.onclick = function () {
+let first = document.getElementById("1st");
+first.addEventListener('click', fontSize = function () {
     html.style.fontSize = "23px";
-}
+});
 
+let second = document.getElementById("2nd");
 second.onclick = function () {
     html.style.fontSize = "20px";
 }
-
-// var loadMore = document.getElementById("LoadMore");
-
-// loadMore.onclick = function () {
-    
-// }
